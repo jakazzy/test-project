863f4ab6d1a7831cb973a6b24b010c197e723984
jakazzy <jakazzy@gmail.com>
2020-07-23 15:20:50 +0000

    add the radio buttons and the digita input field

--- src/StepOne.jsx
+++ src/StepOne.jsx
@@ -5,17 +5,55 @@ function StepOne() {
   return (
     <div className="container personal-info">
       <form>
-        <div class="form-group">
+        <div className="form-group">
           <label htmlFor="name">Name of skilled craft person:</label>
-          <input type="name" class="form-control" id="name" />
+          <input type="name" className="form-control" id="name" />
         </div>
-        <div class="form-group">
+
+        <fieldset className="form-group">
+          <div className="row">
+            <legend className="col-form-label col-sm-2 pt-0">Gender</legend>
+            <div className="col-sm-10">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="gridRadios"
+                  id="gridRadios1"
+                  value="option1"
+                  checked
+                />
+                <label className="form-check-label" htmlFor="male">
+                  Male
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="radio"
+                  name="gridRadios"
+                  id="gridRadios2"
+                  value="option2"
+                />
+                <label className="form-check-label" htmlFor="female">
+                  Female
+                </label>
+              </div>
+            </div>
+          </div>
+        </fieldset>
+
+        <div className="form-group">
           <label htmlFor="tel">Contact Number:</label>
-          <input type="tel" class="form-control" id="tel" />
+          <input type="tel" className="form-control" id="tel" />
         </div>
-        <div class="form-group">
-          <label for="email">Email address:</label>
-          <input type="email" class="form-control" id="email" />
+        <div className="form-group">
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" className="form-control" id="email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Ghana Post Digital Address:</label>
+          <input type="address" className="form-control" id="address" />
         </div>
         {/* 
         <div class="checkbox">
@@ -23,7 +61,7 @@ function StepOne() {
             <input type="checkbox" /> Remember me
           </label>
         </div> */}
-        <button type="submit" class="btn btn-default">
+        <button type="submit" className="btn btn-default">
           Submit
         </button>
       </form>
