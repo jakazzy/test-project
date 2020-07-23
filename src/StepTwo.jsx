df65d4b113a02312fb873b8fc2af60a0dd15dd4a
jakazzy <jakazzy@gmail.com>
2020-07-23 15:27:38 +0000

    fix error with the step two component

--- src/StepTwo.jsx
+++ src/StepTwo.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 const StepTwo = () => {
-  return function StepOne() {
-    return (
-      <div>
-        <form>
-          <label htmlFor="name"> Name</label>
-          <input type="text" id="name" />
-        </form>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <form>
+        <label htmlFor="name"> Name</label>
+        <input type="text" id="name" />
+      </form>
+    </div>
+  );
 };
 
 export default StepTwo;
