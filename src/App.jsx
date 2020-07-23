97fdcd12a947850e747ea3d903d7751537865a6a
jakazzy <jakazzy@gmail.com>
2020-07-23 14:10:56 +0000

    add Bootstrap for styling

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./Form";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
